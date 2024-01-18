54f6066bbb103db32a45840982d16bbc3c9f3b88
Lenovo-User <Lenovo-User@DESKTOP-T3K84PN>
2024-01-18 06:20:20 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        ComboBox1 = New ComboBox()
+        Male = New RadioButton()
+        Female = New RadioButton()
+        RadioButton1 = New RadioButton()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(161, 79)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(39, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(161, 109)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(54, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Surname"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(161, 133)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(28, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Age"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(161, 158)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(62, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "Profession"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(161, 186)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(45, 15)
+        Label5.TabIndex = 4
+        Label5.Text = "Gender"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(161, 276)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(128, 23)
+        Button1.TabIndex = 5
+        Button1.Text = "Welcome Button"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(337, 276)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 6
+        Button2.Text = "Close"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Engineer", "Lecture", "Analyst"})
+        ComboBox1.Location = New Point(245, 158)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 9
+        ' 
+        ' Male
+        ' 
+        Male.AutoSize = True
+        Male.Location = New Point(245, 184)
+        Male.Name = "Male"
+        Male.Size = New Size(51, 19)
+        Male.TabIndex = 10
+        Male.TabStop = True
+        Male.Text = "Male"
+        Male.UseVisualStyleBackColor = True
+        ' 
+        ' Female
+        ' 
+        Female.AutoSize = True
+        Female.Location = New Point(245, 209)
+        Female.Name = "Female"
+        Female.Size = New Size(63, 19)
+        Female.TabIndex = 11
+        Female.TabStop = True
+        Female.Text = "Female"
+        Female.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(245, 234)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(55, 19)
+        RadioButton1.TabIndex = 12
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Other"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(245, 130)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(121, 23)
+        TextBox1.TabIndex = 13
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(245, 105)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(121, 23)
+        TextBox2.TabIndex = 14
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(245, 76)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(121, 23)
+        TextBox3.TabIndex = 15
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Red
+        Button3.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(195, 12)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(357, 36)
+        Button3.TabIndex = 16
+        Button3.Text = "Welcome to Mindworx LMS"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Blue
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button3)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(RadioButton1)
+        Controls.Add(Female)
+        Controls.Add(Male)
+        Controls.Add(ComboBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Sign in on LMS"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Male As RadioButton
+    Friend WithEvents Female As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button3 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles Male.CheckedChanged
+
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show("Malaleng is a software develop at Absa")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hello,you just signed up!")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
